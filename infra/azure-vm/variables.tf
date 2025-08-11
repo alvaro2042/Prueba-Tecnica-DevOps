53874c8441c96e6158b4420c8bb71fd3c589557a
alvaro2042 <128509813+alvaro2042@users.noreply.github.com>
2025-08-10 21:12:32 -0500

    Create variables.tf

---
+++ infra/azure-vm/variables.tf
@@ -0,0 +1,5 @@
+variable "resource_group_name" { default = "rg-demo" }
+variable "location" { default = "eastus" }
+variable "name_prefix" { default = "demo" }
+variable "admin_username" { default = "azureuser" }
+variable "public_key_path" { default = "~/.ssh/id_rsa.pub" }
